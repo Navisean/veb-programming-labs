1f1cb610247dfe7387216d813fb11e056d3900ec
Navisean <cinicaivan@gmail.com>
2026-03-06 00:11:13 +0300

    lab2

---
+++ lab2/triangle.js
@@ -0,0 +1,7 @@
+let k1 = parseFloat(prompt("Введите первый катет"));
+
+let k2 = parseFloat(prompt("Введите второй катет"));
+	
+let g = Math.sqrt(Math.pow(k1, 2) + Math.pow(k2, 2))
+	
+alert("Гипотенуза ровна: " + g);
